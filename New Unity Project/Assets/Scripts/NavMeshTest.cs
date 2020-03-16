e59de232eed808c383295d54504aa07203f44bde
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-16 19:49:57 +0000

    update1

--- New Unity Project/Assets/Scripts/NavMeshTest.cs
+++ New Unity Project/Assets/Scripts/NavMeshTest.cs
@@ -6,20 +6,42 @@ using UnityEngine.AI;
 public class NavMeshTest : MonoBehaviour
 {
 
-    public GameObject DeathSpheres;
+    GameObject Player;
     Vector3 position;
     NavMeshAgent agent;
+    public Color debuglinecolor = Color.red;
 
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-		
+        agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        //agent.SetDestination(Player.transform.position);
 	}
+
+    private void OnDrawGizmos()
+    {
+        if(agent != null)
+        {
+            if(agent.pathStatus != NavMeshPathStatus.PathInvalid)
+            {
+                for (int i = 0; i < agent.path.corners.Length; i++)
+                {
+                    if(i + 1 < agent.path.corners.Length)
+                    {
+                        Gizmos.color = debuglinecolor;
+                        Gizmos.DrawLine(agent.path.corners[i], agent.path.corners[i + 1]);
+                        Debug.DrawLine(agent.path.corners[i], agent.path.corners[i + 1]);
+                        //print("test");
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ New Unity Project/Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemies;
+    public float Starttime = 0;
+    public float EndTime = 4;
+    Vector3 position;
+    BoxCollider SpawnArea;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnArea = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(enemies,RandomPosition(),Quaternion.identity);
+    }
+
+    public Vector3 RandomPosition()
+    {
+        float x = Random.Range(SpawnArea.bounds.min.x,SpawnArea.bounds.max.x);// getting random position of x in spawn area
+        float y = Random.Range(SpawnArea.bounds.min.y,SpawnArea.bounds.max.y);
+        float z = Random.Range(SpawnArea.bounds.min.z,SpawnArea.bounds.max.z);
+
+
+        
+
+        return new Vector3(x,y,z);
+    }
+}
