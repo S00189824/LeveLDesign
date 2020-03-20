c1a4c9292575e3d66c403104a81d9a099b2e85a9
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-20 23:30:31 +0000

    dgg

---
+++ New Unity Project/Assets/Scripts/GravityPullWeaponTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityPullWeaponTest : RayCastWeapons
+{
+    public float ImpulseAmount = 10;
+    LayerMask objecttobepulled;
+    public float pullableDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Fire(Vector3 FireFromPosition)
+    {
+        
+
+        ShootRay(transform.position, transform.forward);
+
+        
+
+        if (Vector3.Distance(transform.position, raycastHit.collider.transform.position) <= pullableDistance)
+        {
+            if (raycastHit.collider.GetComponent<Rigidbody>())
+            {
+                raycastHit.collider.GetComponent<Rigidbody>().AddForce (ray.direction * ImpulseAmount,ForceMode.Impulse);
+            }
+        }
+    }
+
+    
+}
---
+++ New Unity Project/Assets/Scripts/RayCastWeapons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastWeapons : Weapon
+{
+    protected RaycastHit raycastHit;
+    public float Range;
+    public float DamagePerHit;
+    protected Ray ray;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+
+    protected void ApplyDamage(HealthComponent healthComponent)
+    {
+        healthComponent.ApplyDamage(DamagePerHit);
+    }
+
+    public void ShootRay(Vector3 Weaponpos, Vector3 direction)
+    {
+        ray = new Ray(Weaponpos, direction);
+        Physics.Raycast(ray, out raycastHit);
+
+        //if (Physics.Raycast(Weaponpos, transform.forward, out raycastHit, Range))
+        //{
+        //    HealthComponent health = raycastHit.collider.GetComponent<HealthComponent>();
+        //}
+
+        Debug.DrawRay(ray.origin, ray.direction * 15, Color.red,Mathf.Infinity);
+    }
+}
