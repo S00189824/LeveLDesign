716f6deeb38254000c359e4441347a7c7bd77be8
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-22 22:22:22 +0000

    dfgdfgdf

--- New Unity Project/Assets/Scripts/BlockRotation.cs
+++ New Unity Project/Assets/Scripts/BlockRotation.cs
@@ -6,6 +6,9 @@ public class BlockRotation : MonoBehaviour
 {
 
     private Vector3 rotateVector = Vector3.one;
+    float x;
+    float y;
+    float z;
 
     // Start is called before the first frame update
     void Start()
@@ -21,16 +24,17 @@ public class BlockRotation : MonoBehaviour
 
     private void CalculateRotationVector()
     {
-        float x = Random.Range(-0.5f, 0.5f);
-        float y = Random.Range(-0.5f, 0.5f);
-        float z = Random.Range(-0.5f, 0.5f);
+        x = Random.Range(-0.5f, 0.5f);
+        y = Random.Range(-0.5f, 0.5f);
+        z = Random.Range(-0.5f, 0.5f);
 
         rotateVector = new Vector3(x, y, z);
     }
 
     private void Rotateobject()
     {
-        //transform.Rotate(Vector3.up,2f);
-        transform.eulerAngles += rotateVector;
+        transform.Rotate(rotateVector,2f);
+        
+        //transform.eulerAngles += rotateVector;
     }
 }
